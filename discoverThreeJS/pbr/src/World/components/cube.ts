f36a6e0d2fea4fdf93a474cae41302bd41cc0a92
josephpanossian <josephpanossian@gmail.com>
2023-07-17 13:33:49 -0700

    pbr proj init

---
+++ discoverThreeJS/pbr/src/World/components/cube.ts
@@ -0,0 +1,12 @@
+import { Mesh, BoxGeometry, MeshBasicMaterial } from "three"
+
+function createCube() {
+  const geometry = new BoxGeometry(2, 2, 2);
+  const material = new MeshBasicMaterial();
+
+  const cube = new Mesh(geometry, material);
+
+  return cube;
+}
+
+export { createCube };
